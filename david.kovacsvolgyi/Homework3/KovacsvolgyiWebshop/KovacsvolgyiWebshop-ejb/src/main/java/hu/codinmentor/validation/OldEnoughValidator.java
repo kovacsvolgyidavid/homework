4f83f328aed5f0131a340d92f41c44ba45fed10f
David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
2016-04-08 08:55:22 +0200

    Add OldEnoughValidator

---
+++ david.kovacsvolgyi/Homework3/KovacsvolgyiWebshop/KovacsvolgyiWebshop-ejb/src/main/java/hu/codinmentor/validation/OldEnoughValidator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.codinmentor.validation;
+
+import java.time.LocalDate;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ *
+ * @author keni
+ */
+public class OldEnoughValidator implements ConstraintValidator<OldEnough, LocalDate >{
+
+    @Override
+    public void initialize(OldEnough constraintAnnotation) {
+        
+    }
+
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+       return value.isBefore(LocalDate.now());
+    }
+    
+}
