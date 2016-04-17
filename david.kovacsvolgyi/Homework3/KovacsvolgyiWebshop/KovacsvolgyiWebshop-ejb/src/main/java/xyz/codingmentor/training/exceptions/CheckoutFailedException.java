a65f1e841f4a2b10eab30c5b186e735e4357f308
David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
2016-04-17 21:09:31 +0200

    Add Exception + Test.txt

---
+++ david.kovacsvolgyi/Homework3/KovacsvolgyiWebshop/KovacsvolgyiWebshop-ejb/src/main/java/xyz/codingmentor/training/exceptions/CheckoutFailedException.java
@@ -0,0 +1,20 @@
+package xyz.codingmentor.training.exceptions;
+
+import javax.ejb.ApplicationException;
+
+/**
+ *
+ * @author David Kovacsvolgyi <kovacsvolgyi.david@gmail.com>
+ */
+@ApplicationException
+public class CheckoutFailedException extends RuntimeException {
+
+    public CheckoutFailedException() {
+        super();
+    }
+
+    public CheckoutFailedException(String msg) {
+        super(msg);
+    }
+    
+}
